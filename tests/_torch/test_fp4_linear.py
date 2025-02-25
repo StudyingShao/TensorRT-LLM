85a53399923bd6dc9e1eff19491f45dd79f26ef1
Jiang Shao <jiangs@nvidia.com>
2025-02-25 14:06:38 +0000

    Fix a import bug in test_fp4_linear.py
    Signed-off-by: Jiang Shao <jiangs@nvidia.com>

--- tests/_torch/test_fp4_linear.py
+++ tests/_torch/test_fp4_linear.py
@@ -3,13 +3,13 @@ import sys
 
 import pytest
 import torch
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from utils.util import skip_pre_blackwell
 
 import tensorrt_llm.quantization.utils.fp4_utils as fp4_utils
 from tensorrt_llm._torch.modules.linear import Linear
 from tensorrt_llm.models.modeling_utils import QuantAlgo, QuantConfig
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 scaling_vector_size = 16
 
